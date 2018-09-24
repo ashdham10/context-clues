41e8173336b99318134f4e79f43baf565629fd53
ashdham10 <ashleydenham10@gmail.com>
2018-09-24 15:37:24 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+var friends = ['London', 'Jordin', 'Amanda', 'Felesha', 'Taylor'];
+var locations = ['office', 'kitchen', 'dining room', 'garage', 
+                'bathroom', 'bedroom', 'living room', 'hallway',
+                 'attic', 'basement'];
+var weapons = ['baseball bat', 'pool noodle', 'knife', 'toothpick', 
+                'gun', 'jello', 'stapler', 'pillow', 'rubber chicken', 
+                'scissors', 'pie', 'umbrella', 'pickle jar', 'rubber duck',
+                'bubbles', 'microwave', 'hammer', 'feather duster', 'vase', 'banana'];
+
+for(var i = 1; i <= 100; i++){
+    
+}
+
+})
