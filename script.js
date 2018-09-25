c94e6d670ea5ed4b264aa1b3ee21c9415d1f48e7
ashdham10 <ashleydenham10@gmail.com>
2018-09-25 14:58:21 -0500

    finished

--- script.js
+++ script.js
@@ -9,31 +9,23 @@ var weapons = ['baseball bat', 'pool noodle', 'knife', 'toothpick',
                 'scissors', 'pie', 'umbrella', 'pickle jar', 'rubber duck',
                 'bubbles', 'microwave', 'hammer', 'feather duster', 'vase', 'banana'];
 
-// var randomFriend = Math.floor(Math.random() * friends.length);
-// var randomLocation = Math.floor(Math.random() * locations.length);
-// var randomWeapon = Math.floor(Math.random() * weapons.length); 
-// var friend = friends[randomFriend];
-// var location = locations[randomLocation];
-// var weapon = weapons[randomWeapon];
-
-function accusationsFunc(){
 for(var i = 1; i <= 100; i++){
-  var accusations = document.createElement('h3');
-  document.body.appendChild(accusations);
-  var headers = document.createTextNode('Accusation ' + [i]);
-  var friend = friends[i %5];
-  var location = locations[i %10];
-  var weapon = weapons[i %20];
-  accusations.appendChild(headers);
-  accusations.addEventListener('click', function clickAlert(){
+  var h3 = document.createElement('h3');
+  document.body.appendChild(h3);
+  h3.innerText = 'Accusation ' + [i];
+
+  function accusationsFunc(){ 
+    var randomFriend = Math.floor(Math.random() * friends.length%5);
+    var randomLocation = Math.floor(Math.random() * locations.length%10);
+    var randomWeapon = Math.floor(Math.random() * weapons.length%20); 
+    var friend = friends[randomFriend];
+    var location = locations[randomLocation];
+    var weapon = weapons[randomWeapon];
+
+  return function clickAlert(){
     alert(`I accuse ${friend} with the ${weapon} in the ${location}!`);
-    
-  });
+  }
 }
+  h3.addEventListener('click',  accusationsFunc());
 }
-
-accusationsFunc();
-
 });
-
-
